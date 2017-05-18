1aaefbae04e192e21cf883cd3b8eafd45c02b1c9
Guy Barrand <guy.barrand@gmail.com>
2017-05-18 11:33:34 +0300

    update

--- applications/MEMPHYS_analysis_aida_ROOT.cxx
+++ applications/MEMPHYS_analysis_aida_ROOT.cxx
@@ -1,16 +1,7 @@
-//
-// Compiled version of the interpreted
-//   ../scripts/ROOT/aida_ROOT.C
-// which uses OpenScienitst/AIDA to read a MEMPHYS.root
-// file and the TH* for the histogramming and plotting.
-//
 
-#include <AIDA/IAnalysisFactory.h>
-#include <AIDA/ITreeFactory.h>
-#include <AIDA/ITree.h>
-#include <AIDA/IManagedObject.h>
-#include <AIDA/ITupleEntry.h>
-#include <AIDA/ITuple.h>
+// A MEMPHYS.root file is read with the AIDA API and plotting is done with CERN-ROOT.
+
+#include "analysis.icc"
 
 #include <TH1D.h>
 #include <TH2D.h>
@@ -18,18 +9,69 @@
 #include <TSystem.h>
 #include <TApplication.h>
 
-#include <iostream>
+#ifdef APP_USE_ARCHIVE
+#include <BatchLab/Core/Main.h>
+extern "C" {
+  void BatchLabRioInitialize(Slash::Core::ISession&);
+}
+#endif
 
-#include "../scripts/ROOT/aida_ROOT.C"
+#include <iostream>
 
 int main(int aArgc,char* aArgv[]) {
   //ROOT::GetROOT();
   
   new TApplication("MEMPHYS_analysis_aida_ROOT",&aArgc,aArgv);
 
-  aida_ROOT();
+  ////////////////////////////////////////////////////////
+  // Create histograms : /////////////////////////////////
+  ////////////////////////////////////////////////////////
+  TH1D* hits_times = new TH1D("hits_times","Hits times",100,0,3000);
+
+  TH2D* digits_time_pe = new TH2D("digits_pe_time","Digits PE time",100,0,3000,100,0,10);
+
+  ////////////////////////////////////////////////////////
+  /// Read data : ////////////////////////////////////////
+  ////////////////////////////////////////////////////////
+#ifdef APP_USE_ARCHIVE
+  BatchLab::Main* session = new BatchLab::Main(std::vector<std::string>());
+  Slash::Core::ILibraryManager* libraryManager = Slash::libraryManager(*session);
+  if(!libraryManager) {std::cout << "LibraryManager not found." << std::endl;return EXIT_FAILURE;}
+  Slash::Core::ILibrary* library = libraryManager->addLibrary("Rio","main","");
+  if(!library) {std::cout << "addLibrary() failed." << std::endl;return EXIT_FAILURE;}
+  ::BatchLabRioInitialize(*session);
+  AIDA::IAnalysisFactory* aida = static_cast<AIDA::IAnalysisFactory*>(session);
+#else
+  AIDA::IAnalysisFactory* aida = AIDA_createAnalysisFactory();
+#endif
+  if(!aida) {
+    std::cout << "AIDA not found." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if(!read_data(*aida,*hits_times,*digits_time_pe)) {
+    std::cout << "can't read data file." << std::endl;
+    delete aida;
+    return EXIT_FAILURE;
+  }
+
+  delete aida;
+
+  ////////////////////////////////////////////////////////
+  /// plot histos : //////////////////////////////////////
+  ////////////////////////////////////////////////////////
+  TCanvas* plotter = new TCanvas("canvas","",10,10,800,600);
+  plotter->Divide(1,2);  
+
+  plotter->cd(1);
+  hits_times->Draw();
+
+  plotter->cd(2);
+  digits_time_pe->Draw();
+
+  plotter->Update();
 
   gSystem->Run();
 
-  return 1;
+  return EXIT_SUCCESS;
 }
