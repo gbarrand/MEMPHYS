5d4c0edd51da96e174b883e0ccd60b2486145d1f
Guy Barrand <guy.barrand@gmail.com>
2017-05-18 10:50:13 +0300

    update

--- applications/MEMPHYS_analysis.cxx
+++ applications/MEMPHYS_analysis.cxx
@@ -14,6 +14,9 @@ inline std::string tostring(double a_value) {
 }
 }}
 
+#include <inlib/histo/h1d>
+#include <inlib/histo/h2d>
+
 #include <iostream>
 
 //////////////////////////////////////////////////////////////////////////////
@@ -105,7 +108,7 @@ inline bool dump_tracks(AIDA::ITuple& aParent) {
   return true;
 }
 
-inline bool process_hits_times(AIDA::ITupleEntry* a_entry,AIDA::IHistogram1D& aHisto) {
+inline bool process_hits_times(AIDA::ITupleEntry* a_entry,inlib::histo::h1d& aHisto) {
   if(!a_entry) return false;
   AIDA::ITuple* tuple = cast_Tuple(a_entry);
   if(!tuple) return false;
@@ -120,7 +123,7 @@ inline bool process_hits_times(AIDA::ITupleEntry* a_entry,AIDA::IHistogram1D& aH
   return true;
 }
 
-inline bool process_hits(AIDA::ITupleEntry* a_entry,AIDA::IHistogram1D& aHisto) {
+inline bool process_hits(AIDA::ITupleEntry* a_entry,inlib::histo::h1d& aHisto) {
   if(!a_entry) return false;
 
   AIDA::ITuple* tuple = cast_Tuple(a_entry);
@@ -131,13 +134,13 @@ inline bool process_hits(AIDA::ITupleEntry* a_entry,AIDA::IHistogram1D& aHisto) 
 
     //int tubeId = tuple->getInt(0);
     //int totalPE = tuple->getInt(1);
-
+    
     if(!process_hits_times((AIDA::ITupleEntry*)tuple->getObject(2),aHisto)) return false;
   }
   return true;
 }
 
-inline bool process_digits(AIDA::ITupleEntry* a_entry,AIDA::IHistogram2D& aHisto) {
+inline bool process_digits(AIDA::ITupleEntry* a_entry,inlib::histo::h2d& aHisto) {
   if(!a_entry) return false;
   AIDA::ITuple* tuple = cast_Tuple(a_entry);
   if(!tuple) return false;
@@ -155,7 +158,9 @@ inline bool process_digits(AIDA::ITupleEntry* a_entry,AIDA::IHistogram2D& aHisto
   return true;
 }
 
-#ifdef APP_USE_ARCHIVE //plotting done with inlib/exlib.
+//////////////////////////////////////////////////////////////////////////////
+/// plotting done with inlib/exlib : //////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 #ifdef _WIN32
 #include <exlib/Windows/plotter>
 #define EXLIB_SCREEN_MGR Windows
@@ -163,7 +168,6 @@ inline bool process_digits(AIDA::ITupleEntry* a_entry,AIDA::IHistogram2D& aHisto
 #include <exlib/X11/plotter>
 #define EXLIB_SCREEN_MGR X11
 #endif
-#include <exlib/AIDA/h2plot>
 inline void set_region_style(inlib::sg::plotter& a_plotter) {
   
   a_plotter.right_axis_visible = true;
@@ -271,7 +275,7 @@ inline void set_region_style(inlib::sg::plotter& a_plotter) {
   //gStyle->SetLabelSize(0.05,"XYZ"); //ROOT def 0.04. SoPlotter def 0.014.  
   
 }
-inline bool plot(AIDA::IAnalysisFactory&,AIDA::IHistogram1D& aHisto1D,AIDA::IHistogram2D& aHisto2D) {
+inline bool plot(inlib::histo::h1d& aHisto1D,inlib::histo::h2d& aHisto2D) {
   
   exlib::EXLIB_SCREEN_MGR::session smgr(std::cout);
   if(!smgr.is_valid()) return false;
@@ -286,7 +290,7 @@ inline bool plot(AIDA::IAnalysisFactory&,AIDA::IHistogram1D& aHisto1D,AIDA::IHis
     set_region_style(sgp);
     sgp.x_axis().title = "time";
     sgp.y_axis().title = "Entries";
-    inlib::sg::plottable* ptb = new exlib::AIDA::h1d2plot(aHisto1D);
+    inlib::sg::plottable* ptb = new inlib::sg::h1d2plot(aHisto1D);
     ptb->set_name(aHisto1D.title());
     sgp.add_plottable(ptb);
   }
@@ -296,7 +300,7 @@ inline bool plot(AIDA::IAnalysisFactory&,AIDA::IHistogram1D& aHisto1D,AIDA::IHis
     set_region_style(sgp);
     sgp.x_axis().title = "time";
     sgp.y_axis().title = "PE";
-    inlib::sg::plottable* ptb = new exlib::AIDA::h2d2plot(aHisto2D);
+    inlib::sg::plottable* ptb = new inlib::sg::h2d2plot(aHisto2D);
     ptb->set_name(aHisto2D.title());
     sgp.add_plottable(ptb);
   }
@@ -307,157 +311,6 @@ inline bool plot(AIDA::IAnalysisFactory&,AIDA::IHistogram1D& aHisto1D,AIDA::IHis
 
   return true;
 }
-#else
-inline void set_region_style(AIDA::IPlotterRegion& aRegion) {
-  // Taken from G4SPL_analysis.cxx.
-  //FIXME : have to do the below with AIDA styles.
-
-  // ROOT is in NDC, then we take a plotter with width = height = 1
-
-  // PAW :
-  //float XSIZ = 20.0F;
-  //float XLAB = 1.4F;
-  //float YSIZ = 20.0F;
-  //float YLAB = 1.4F;
-
-  float XLAB = 1.4F/20.0F; //0.07 //x distance of y title to data frame.
-  float YLAB = 0.8F/20.0F; //0.04 //y distance of x title to data frame.
-  YLAB = 0.05F; //FIXME : cooking.
-
-  // To have a good matching with ROOT for text size :
-  //double majic = 0.014/0.027;
-  double majic = 0.6;
-  std::string s = Lib::smanip::tostring(majic);
-  //FIXME : have methods for the below :
-  //aRegion.style().setParameter("textScale",s);
-  aRegion.style().xAxisStyle().setParameter("magStyle.scale",s);
-  aRegion.style().yAxisStyle().setParameter("magStyle.scale",s);
-  aRegion.style().xAxisStyle().tickLabelStyle().setParameter("scale",s);
-  aRegion.style().xAxisStyle().labelStyle().setParameter("scale",s);
-  aRegion.style().yAxisStyle().tickLabelStyle().setParameter("scale",s);
-  aRegion.style().yAxisStyle().labelStyle().setParameter("scale",s);
-
-  // ROOT def margins 0.1. SoPlotter def 0.1.
-  aRegion.layout().setParameter("rightMargin",0.1);
-  aRegion.layout().setParameter("topMargin",0.1);
-  //gStyle->SetPadBottomMargin(0.15);
-  aRegion.layout().setParameter("leftMargin",0.15);
-  //gStyle->SetPadLeftMargin(0.15);
-  aRegion.layout().setParameter("bottomMargin",0.15);
-
-  aRegion.style().setParameter("superposeBins","TRUE");
-  aRegion.setParameter("plotter.wallStyle.visible","FALSE");
-  aRegion.setParameter("plotter.gridStyle.visible","FALSE");
-
-  //std::string font = "Hershey"; //PAW font.
-  //std::string font = "TTF/arialbd"; //Default ROOT font 62.
-  //std::string font = "TTF/couri";
-  std::string font = "TTF/times"; //= ROOT 132 ?
-  //std::string smoothing = "FALSE";
-  std::string smoothing = "TRUE";
-
-  // X axis :
-  aRegion.style().xAxisStyle().tickLabelStyle().setFont(font);
-  aRegion.style().xAxisStyle().tickLabelStyle().setParameter("smoothing",smoothing);
-  aRegion.style().xAxisStyle().labelStyle().setFont(font);
-  aRegion.style().xAxisStyle().labelStyle().setParameter("smoothing",smoothing);
-  aRegion.setParameter("plotter.xAxis.magStyle.fontName",font);
-  aRegion.setParameter("plotter.xAxis.magStyle.smoothing",smoothing);
-  aRegion.style().xAxisStyle().lineStyle().setThickness(2);
-  aRegion.setParameter("plotter.xAxis.ticksStyle.width","2");
-  aRegion.style().setParameter("topAxisVisible","TRUE");
-  // Set hplot tick modeling :
-  aRegion.style().xAxisStyle().setParameter("modeling","hplot");
-  aRegion.style().xAxisStyle().setParameter("divisions","505");
-
-  // Y axis :
-  //aRegion.style().setParameter("yAxisLogScale","TRUE");
-  aRegion.style().yAxisStyle().tickLabelStyle().setFont(font);
-  aRegion.style().yAxisStyle().tickLabelStyle().setParameter("smoothing",smoothing);
-  aRegion.style().yAxisStyle().labelStyle().setFont(font);
-  aRegion.style().yAxisStyle().labelStyle().setParameter("smoothing",smoothing);
-  aRegion.setParameter("plotter.yAxis.magStyle.fontName",font);
-  aRegion.setParameter("plotter.yAxis.magStyle.smoothing",smoothing);
-  aRegion.style().yAxisStyle().lineStyle().setThickness(2);
-  aRegion.setParameter("plotter.yAxis.ticksStyle.width","2");
-
-  //gStyle->SetTitleSize(0.06,"XYZ"); //ROOT def 0.04. SoPlotter : 0.014
-  aRegion.setParameter("plotter.xAxis.titleHeight","0.06");
-  aRegion.setParameter("plotter.yAxis.titleHeight","0.06");
-  //gStyle->SetLabelOffset(0.01,"Y"); //ROOT def 0.005. SoPlotter def 0.02
-  aRegion.setParameter("plotter.yAxis.labelToAxis","0.01");
-  //gStyle->SetTitleOffset(1.1,"Y");
-  aRegion.setParameter("plotter.yAxis.titleToAxis",Lib::smanip::tostring(1.1*XLAB));
-  aRegion.setParameter("plotter.xAxis.titleToAxis",Lib::smanip::tostring(1.1*YLAB));
-  //gStyle->SetLabelSize(0.05,"XYZ"); //ROOT def 0.04. SoPlotter def 0.014.
-  aRegion.setParameter("plotter.yAxis.labelHeight","0.05");
-  aRegion.setParameter("plotter.xAxis.labelHeight","0.05");
-
-  aRegion.style().setParameter("rightAxisVisible","TRUE");
-  // Set hplot tick modeling :
-  aRegion.style().yAxisStyle().setParameter("modeling","hplot");
-  aRegion.style().yAxisStyle().setParameter("divisions","505");
-
-  // title :
-  aRegion.style().setParameter("titleHeight","0.04");
-  aRegion.style().setParameter("titleToAxis","0.02");
-  aRegion.style().titleStyle().textStyle().setFont(font);
-  aRegion.style().titleStyle().textStyle().setParameter("smoothing",smoothing);
-  aRegion.style().titleStyle().textStyle().setColor("0 0 1");
-
-  // legend box :
-  aRegion.setParameter("legendRegionVisible","TRUE");
-  aRegion.setParameter("legendRegion.viewportRegion.backgroundColor","1 1 0");
-  aRegion.setParameter("legendRegionSize","0.15 0.16");
-  aRegion.setParameter("legendRegionOrigin","0.1 0.1");
-  aRegion.setParameter("legendRegion.horizontalMargin","2");
-  aRegion.setParameter("legendRegion.verticalMargin","2");
-
-  // Frame :
-  aRegion.setParameter("plotter.innerFrameStyle.lineWidth","2");
-}
-inline bool plot(AIDA::IAnalysisFactory& aAIDA,AIDA::IHistogram1D& aHisto1D,AIDA::IHistogram2D& aHisto2D) {
-
-  AIDA::IPlotterFactory* plotterFactory = 
-    aAIDA.createPlotterFactory(0,0);
-  if(!plotterFactory) {
-    std::cout << "can't get a PlotterFactory." << std::endl;
-    return false;
-  }
-    
-  AIDA::IPlotter* plotter = plotterFactory->create();
-  if(!plotter) {
-    std::cout << "can't get a plotter." << std::endl;
-    return false;
-  }
-    
-  plotter->clearRegions();
-  plotter->createRegions(1,2,0);
-
- {plotter->setCurrentRegionNumber(0);
-  AIDA::IPlotterRegion& region = plotter->currentRegion();
-  set_region_style(region);
-  region.style().xAxisStyle().setLabel("time");
-  region.style().yAxisStyle().setLabel("Entries");
-  region.plot(aHisto1D);}
-
- {plotter->setCurrentRegionNumber(1);
-  AIDA::IPlotterRegion& region = plotter->currentRegion();
-  set_region_style(region);
-  region.style().xAxisStyle().setLabel("time");
-  region.style().yAxisStyle().setLabel("PE");
-  region.plot(aHisto2D);}
-
-  plotter->show();
-
-  plotter->interact();
-
-  delete plotter;
-  delete plotterFactory;
-
-  return true;
-}
-#endif
 
 #ifdef APP_USE_ARCHIVE
 #include <BatchLab/Core/Main.h>
@@ -494,42 +347,16 @@ int main(int aArgc,char** aArgv) {
   }
 
   ////////////////////////////////////////////////////////
-  // Create histograms in memory tree ////////////////////
+  // Create histograms : /////////////////////////////////
   ////////////////////////////////////////////////////////
-  AIDA::ITree* memory = treeFactory->create();
-  if(!memory) {
-    std::cout << "can't get memory tree." << std::endl;
-    return EXIT_FAILURE;
-  }
-  AIDA::IHistogramFactory* histogramFactory = aida->createHistogramFactory(*memory);
-  if(!histogramFactory) {
-    std::cout << "can't get an histogram factory." << std::endl;
-    return EXIT_FAILURE;
-  }
-  
-  AIDA::IHistogram1D* hits_times = histogramFactory->createHistogram1D("hits_times","Hits times",100,0,3000);
-  if(!hits_times) {
-    std::cout << "can't create histogram : time." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  AIDA::IHistogram2D* digits_time_pe = 
-    histogramFactory->createHistogram2D("digits_pe_time","Digits PE time",
-					100,0,3000,100,0,10);
-  if(!digits_time_pe) {
-    std::cout << "can't create histogram : digits_time_pe." << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  delete histogramFactory;
+  inlib::histo::h1d hits_times("Hits times",100,0,3000);
+  inlib::histo::h2d digits_time_pe("Digits PE time",100,0,3000,100,0,10);
 
   ////////////////////////////////////////////////////////
   /// Read data //////////////////////////////////////////
   ////////////////////////////////////////////////////////
 
-  std::string fmt = (aArgc>=2?aArgv[1]:"root"); //file extension
-
-  AIDA::ITree* tree = treeFactory->create("MEMPHYS."+fmt,fmt,true,false);
+  AIDA::ITree* tree = treeFactory->create("MEMPHYS.root","root",true,false);
   if(!tree) {
     std::cout << "can't open data file." << std::endl;
     return EXIT_FAILURE;
@@ -594,19 +421,18 @@ int main(int aArgc,char** aArgv) {
 
     //if(!dump_tracks(*tuple)) return EXIT_FAILURE;
 
-    if(!process_hits((AIDA::ITupleEntry*)tuple->getObject(10),*hits_times)) return EXIT_FAILURE;
+    if(!process_hits((AIDA::ITupleEntry*)tuple->getObject(10),hits_times)) return EXIT_FAILURE;
     
-    if(!process_digits((AIDA::ITupleEntry*)tuple->getObject(13),*digits_time_pe)) return EXIT_FAILURE;
+    if(!process_digits((AIDA::ITupleEntry*)tuple->getObject(13),digits_time_pe)) return EXIT_FAILURE;
 
     irow++;
   }
 
   delete tree;
   delete treeFactory;
-
-  plot(*aida,*hits_times,*digits_time_pe);
-
   delete aida;
+
+  plot(hits_times,digits_time_pe);
 
   return EXIT_SUCCESS;
 }
