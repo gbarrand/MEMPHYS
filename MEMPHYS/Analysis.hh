1b4d7e4dc1db5452dc054279796f1fb7512d011a
Guy Barrand <guy.barrand@gmail.com>
2017-05-24 16:58:50 +0200

    update

--- MEMPHYS/Analysis.hh
+++ MEMPHYS/Analysis.hh
@@ -11,6 +11,11 @@ namespace AIDA {
  class ITree;
 }
 
+#ifdef APP_USE_INLIB_WROOT
+#include <inlib/wroot/file>
+#include <inlib/wroot/tree>
+#endif
+
 //JEC 10/1/06 introduction
 namespace MEMPHYS {
 
@@ -21,14 +26,28 @@ class Analysis  : public virtual IAppManager {
   public:        
     Analysis(AIDA::IAnalysisFactory*,bool aBatch = true);
     virtual ~Analysis();
-
+  protected:
+#ifdef APP_USE_INLIB_WROOT
+    Analysis(const Analysis& a_from):m_file(a_from.m_file.out(),"") {}
+#endif
+    Analysis& operator=(const Analysis& a_from) {return *this;}
+  public:
     //Get tree pointer
     AIDA::ITree* tree() const {return fTree;}
-
+  
   private:
-    AIDA::IAnalysisFactory* fAIDA;
     bool fBatch;
+    AIDA::IAnalysisFactory* fAIDA;
     AIDA::ITree* fTree;
+#ifdef APP_USE_INLIB_WROOT
+  public:
+    inlib::wroot::file m_file;
+    inlib::wroot::tree* m_geom_tree;
+    inlib::wroot::leaf<double>* m_leaf_wcRadius;
+    inlib::wroot::leaf<double>* m_leaf_wcLength;
+    inlib::wroot::leaf<double>* m_leaf_pmtRadius;
+    inlib::wroot::leaf<int>* m_leaf_nPMTs;
+#endif  
 };
 
 }
