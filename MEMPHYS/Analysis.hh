79a7a3d238313a706c3b5ef539fdf92d9d9ed201
Guy Barrand <guy.barrand@gmail.com>
2017-05-27 13:23:22 +0200

    update

--- MEMPHYS/Analysis.hh
+++ MEMPHYS/Analysis.hh
@@ -52,39 +52,8 @@ class Analysis  : public virtual IAppManager {
   public:
     inlib::wroot::file m_file;
   public:
-#include "event_tree_h.icc"
-  public:
-    inlib::wroot::tree* m_geom_tree;
-    inlib::wroot::leaf<double>* m_leaf_wcRadius;
-    inlib::wroot::leaf<double>* m_leaf_wcLength;
-
-      inlib::wroot::tree* m_wcOffset_tree;
-      inlib::wroot::leaf<double>* m_wcOffset_leaf_x;
-      inlib::wroot::leaf<double>* m_wcOffset_leaf_y;
-      inlib::wroot::leaf<double>* m_wcOffset_leaf_z;  
-    inlib::wroot::leaf_object* m_leaf_wcOffset;
-  
-    inlib::wroot::leaf<double>* m_leaf_pmtRadius;
-    inlib::wroot::leaf<int>* m_leaf_nPMTs;
-
-      inlib::wroot::tree* m_pmtInfos_tree;
-      inlib::wroot::leaf<int>* m_pmtInfos_leaf_pmtId;
-      inlib::wroot::leaf<int>* m_pmtInfos_leaf_pmtLocation;
-
-        inlib::wroot::tree* m_pmtOrient_tree;
-        inlib::wroot::leaf<double>* m_pmtOrient_leaf_dx;
-        inlib::wroot::leaf<double>* m_pmtOrient_leaf_dy;
-        inlib::wroot::leaf<double>* m_pmtOrient_leaf_dz;
-      inlib::wroot::leaf_object* m_pmtInfos_leaf_Orient;
-
-        inlib::wroot::tree* m_pmtPosition_tree;
-        inlib::wroot::leaf<double>* m_pmtPosition_leaf_x;
-        inlib::wroot::leaf<double>* m_pmtPosition_leaf_y;
-        inlib::wroot::leaf<double>* m_pmtPosition_leaf_z;
-      inlib::wroot::leaf_object* m_pmtInfos_leaf_Position;
-
-    inlib::wroot::leaf_object* m_leaf_pmtInfos;
-  
+#include "Event_tree_h.icc"
+#include "Geometry_tree_h.icc"
 #endif  
 };
 
