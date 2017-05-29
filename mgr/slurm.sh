b122dc9fd68c5e4921224e279ba4ac4812388563
Guy Barrand <guy.barrand@gmail.com>
2017-05-29 14:34:39 +0200

    update

--- mgr/slurm.sh
+++ mgr/slurm.sh
@@ -8,4 +8,7 @@ echo "we are on the node :"
 echo "hostname : `hostname`"
 echo "whoami : `whoami`"
 echo "pwd : `pwd`"
+echo "SLURM_ARRAY_JOB_ID : `printenv SLURM_ARRAY_JOB_ID`"
+echo "SLURM_ARRAY_TASK_ID : `printenv SLURM_ARRAY_TASK_ID`"
+echo "SLURM_ARRAY_TASK_COUNT : `printenv SLURM_ARRAY_TASK_COUNT`"
 sleep 1m
