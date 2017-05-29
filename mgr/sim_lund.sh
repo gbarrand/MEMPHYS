9ebd84bd6b4ca704fb554fa349e13be94dfe6a0b
Guy Barrand <guy.barrand@gmail.com>
2017-05-29 13:52:58 +0200

    update

--- mgr/sim_lund.sh
+++ mgr/sim_lund.sh
@@ -8,4 +8,14 @@ echo "we are on the node :"
 echo "hostname : `hostname`"
 echo "whoami : `whoami`"
 echo "pwd : `pwd`"
+
+G4DATA=/nfs/users/gbarrand/usr/local/geant4/data_82
+NeutronHPCrossSections="${G4DATA}/G4NDL";export NeutronHPCrossSections
+G4LEVELGAMMADATA="${G4DATA}/PhotonEvaporation2";export G4LEVELGAMMADATA
+G4RADIOACTIVEDATA="${G4DATA}/RadioactiveDecay";export G4RADIOACTIVEDATA
+G4LEDATA="${G4DATA}/G4EMLOW";export G4LEDATA
+
+cd ../data
+../mgr/bin_gnu/MEMPHYS_sim 
+
 sleep 1m
