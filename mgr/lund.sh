ab76e54d177d408fe5f5ecf4b957031c4f5edba9
Guy Barrand <guy.barrand@gmail.com>
2017-05-29 12:58:40 +0200

    update

--- mgr/lund.sh
+++ mgr/lund.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH -J "simple job"
 #SBATCH --time=1
-echo "we are on the node"
-hostname
-who
+echo "we are on the node :"
+echo "hostname : `hostname`"
+echo "whoami : `whoami`"
 sleep 2m
